63cd10a992e2ea3dc5a6b29a1fa1826d3df090d7
mah-arch <veeramaheshbathula@gmail.com>
2024-08-21 13:23:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class Helloblue{
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello blue!");
   }
 }
